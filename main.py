adc628ef77fc57d4f1aa9076149df06225ae9a72
sadinenisaisrija-11 <saisrijasadineni@gmail.com>
2025-03-21 12:45:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-
 import streamlit as st
 
-st.title('Hello World')
-st.write('This is a simple Streamlit app.')
+
+name = st.text_input("Enter your name", "John Doe")
+password = st.text_input("Enter your password", type="password")
+
+
+if st.button("Submit"):
+   if len(password) < 8:
+       st.warning("Password must be at least 8 characters long")
+
+
+   st.success("Password is valid")
+
 
